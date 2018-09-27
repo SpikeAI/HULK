cb3211d8f043e13e59d28941296fb0cd60df4ec8
Laurent Perrinet <laurent.perrinet@gmail.com>
2018-09-27 17:50:32 +0200

    re-running  notebooks

--- model.py
+++ model.py
@@ -3,7 +3,7 @@
 from shl_scripts.shl_experiments import SHL, prun
 # pre-loading data
 datapath = '../../SparseHebbianLearning/database'
-opts = dict(datapath=datapath, verbose=0)
+opts = dict(eta=0.0033, eta_homeo=0.05, alpha_homeo=2.5., cache_dir='cache_dir_42', datapath=datapath, verbose=0)
 shl = SHL(**opts)
 data = shl.get_data(matname='data')
 
@@ -19,17 +19,19 @@ import sys
 try:
     n_jobs = sys.argv[2]
 except:
-    n_jobs = 1
     n_jobs = 4
     n_jobs = 9
     n_jobs = 10
     n_jobs = 35
+    n_jobs = 1
 
 list_figures = []
 
 from shl_scripts.shl_experiments import SHL_set
 for homeo_method in homeo_methods:
-    experiments = SHL_set(dict(homeo_method=homeo_method, datapath=datapath), tag=tag + '_' + homeo_method, N_scan=N_cv)
+    opts_ = opts.copy()
+    opts_.update(homeo_method=homeo_method)
+    experiments = SHL_set(opts_, tag=tag + '_' + homeo_method, N_scan=N_cv)
     experiments.run(variables=['seed'], n_jobs=n_jobs, verbose=0)
 
 # Figure 2-B
@@ -38,12 +40,15 @@ variables = ['eta', 'alpha_homeo', 'eta_homeo', 'l0_sparseness']
 variables = ['eta', 'alpha_homeo', 'eta_homeo']
 
 for homeo_method in homeo_methods:
-    experiments = SHL_set(dict(homeo_method=homeo_method, datapath=datapath), tag=tag + '_' + homeo_method)
+    opts_ = opts.copy()
+    opts_.update(homeo_method=homeo_method)
+    experiments = SHL_set(opts_, tag=tag + '_' + homeo_method)
     experiments.run(variables=variables, n_jobs=n_jobs, verbose=0)
 
 # Annex X.X
 for algorithm in ['lasso_lars', 'lasso_cd', 'lars', 'omp', 'mp']: # 'threshold',
-    opts = dict(homeo_method='None', learning_algorithm=algorithm, verbose=0)
-    shl = SHL(opts)
+    opts_ = opts.copy()
+    opts_.update(homeo_method='None', learning_algorithm=algorithm, verbose=0)
+    shl = SHL(**opts_)
     dico= shl.learn_dico(data=data, list_figures=[],
                    matname=tag + ' - algorithm={}'.format(algorithm))
